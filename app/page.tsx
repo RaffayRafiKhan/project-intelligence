15240bc40b28bc383a4aeccf522db934f28d376c
RaffayRafiKhan <raffayrafikhan@gmail.com>
2026-01-16 19:13:17 +0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Product Intelligence Platform</h1>
+      <p>Backend is under construction.</p>
+    </main>
+  );
+}
